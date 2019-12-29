7f92cafadafda8445ed814bc234733abc6964843
Ryuichi Watanabe <ryucrosskey@gmail.com>
2019-12-30 02:34:45 +0900

    Update age.go

--- src/age.go
+++ src/age.go
@@ -12,13 +12,11 @@ import (
 	"golang.org/x/crypto/ssh/terminal"
 ) decrypt(keys []string, in io.Reader, out io.Writer) {
 	identities := []age.Identity{
-		// If there is an scrypt recipient (it will have to be the only one and)
-		// this identity will be invoked.
+
 		&LazyScryptIdentity{passphrasePrompt},
 	}
 
-	// TODO: use the default location if no arguments are provided:
-	// os.UserConfigDir()/age/keys.txt, ~/.ssh/id_rsa, ~/.ssh/id_ed25519
+
 	for _, name := range keys {
 		ids, err := parseIdentitiesFile(name)
 		if err != nil {
@@ -38,6 +36,5 @@ import (
 
 func logFatalf(format string, v ...interface{}) {
 	_log.Printf(format, v...)
-	_log.Fatalf("[ Did age not do what you expected? Could an error be more useful?" +
-		" Tell us: https://filippo.io/age/report ]")
+
 }
